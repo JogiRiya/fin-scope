bfaace474e1c7b0fcb92081ad73411d60ff48617
JogiRiya <riyajogi09@gmail.com>
2025-07-15 16:37:43 +0530

    Update calc.cpp

--- backend/calc.cpp
+++ backend/calc.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    int income = atoi(argv[1]);
+    int expenses = atoi(argv[2]);
+    int savings = income - expenses;
+
+    if (savings > 0)
+        cout << "Great! You're saving ₹" << savings << " per month.";
+    else
+        cout << "Alert! You're overspending by ₹" << -savings << " per month.";
+    return 0;
+}
 
